feebf49381f7dee5163e2ad8ffe45e086ca03209
Xavier <xavi.fortesa@gmail.com>
2024-08-16 09:08:43 +0200

    add workflow action deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://XavierFortesa.github.io/portfolio/'
+  site: 'https://XavierFortesa.github.io/'
 })
