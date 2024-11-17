69ab74b2666c7f1603eda2be7b09d25b0dc40575
Forteza Bernat <xavier.fortezabernat@dxc.com>
2024-11-17 16:45:50 +0100

    add package dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import robotsTxt from 'astro-robots-txt';
 export default defineConfig({
   integrations: [tailwind(), preact(), robotsTxt()],
   site: 'https://seeyouincanada.github.io/',
+  build: {
+    rollupOptions: {
+      external: ['preact', 'preact/hooks'],
+    },
+  },
 });
