501184bce1194a38f7040e34ad4961a8eb61e6f0
Forteza Bernat <xavier.fortezabernat@dxc.com>
2024-11-17 17:22:07 +0100

    static astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import preact from '@astrojs/preact';
-import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-  integrations: [tailwind(), preact(), robotsTxt()],
-  site: 'https://seeyouincanada.github.io/'
+  output: 'static',
+  base: '/', // Cambia esto si tienes un subdirectorio
+  integrations: [],
 });
