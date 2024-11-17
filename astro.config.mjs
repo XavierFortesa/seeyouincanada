02245251a99a62339bed8803b6b0cc0634f30412
Forteza Bernat <xavier.fortezabernat@dxc.com>
2024-11-17 11:20:13 +0100

    init repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://XavierFortesa.github.io/'
+  site: 'https://seeyouincanada.github.io/'
 })
