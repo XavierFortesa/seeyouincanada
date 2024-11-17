1383585dd95da89fe9a388b4d1de1c0f02704792
Forteza Bernat <xavier.fortezabernat@dxc.com>
2024-11-17 16:41:25 +0100

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+import robotsTxt from 'astro-robots-txt';
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://seeyouincanada.github.io/'
-})
+  integrations: [tailwind(), preact(), robotsTxt()],
+  site: 'https://seeyouincanada.github.io/',
+});
